bdb6421de5c97cf226dee07e0799989ec7591f1b
maimouna <nynasy@gmail.com>
2024-12-20 00:01:43 +0100

    Ajout target

--- script/script.js
+++ script/script.js
@@ -80,7 +80,7 @@ function createPortfolioFromJSON() {
                             <h3 class="card-title">${item.title}</h3>
                             <p class="card-text">${item.text}</p>
                             <div class="text-center">
-                                <a href="${item.link}" class="btn btn-success">Lien</a>
+                                <a href="${item.link}" class="btn btn-success" target="_blank">Lien</a>
                             </div>
                         </div>
                     </div>
